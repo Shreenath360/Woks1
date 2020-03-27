7740a474eb58ff8f37f7ca34f945f4e26413af6c
Shreenath360 <60429850+Shreenath360@users.noreply.github.com>
2020-03-27 19:58:50 +0530

    Add files via upload

---
+++ test/DeclareInstantiation.java
@@ -0,0 +1,14 @@
+package exam.com.test;
+
+public class DeclareInstantiation {
+public  static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = { 33, 12, 14, 5 };
+		for (int i = 0; i < a.length; i++)
+			System.out.println(a[i]);
+
+	
+}
+}
+	
+
